0dd249830649d71c5c8ee530d71a3bda85f37ea7
Mohammad Shamchi <shamchimohammad@gmail.com>
2023-09-21 22:36:02 +0300

    Navbar with Button

---
+++ app/components/index.ts
@@ -0,0 +1,11 @@
+// Use as only file to export all the routes 
+import Hero from "./Hero";
+import CustomButton from './CustomButton'
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+export  {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+}
